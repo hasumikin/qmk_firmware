82674ae079cf4c54db1f4f4a77d10b37c2b7efff
HASUMI Hitoshi <hasumikin@gmail.com>
2020-10-21 10:26:55 +0900

    claw44

--- keyboards/claw44/keymaps/hasumikin/keymap.c
+++ keyboards/claw44/keymaps/hasumikin/keymap.c
@@ -44,31 +44,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                 KC_LALT,   LOWER,  KC_SPC, KC_LCTL,    KC_LGUI,  KC_ENT,  RAISE,  KC_RCTL \
-                             //`-----------------------------------'  `-----------------------------------'
-  ),
-
-  [_RAISE] = LAYOUT( \
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_GRV, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_PLUS,  KC_EQL,\
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_TAB, XXXXXXX, XXXXXXX, KC_QUOT, KC_LPRN, KC_LBRC,                      KC_RBRC, KC_RPRN,  KC_EQL, KC_UNDS, KC_PIPE,  KC_GRV,\
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, KC_DQUO, KC_LCBR, KC_LABK,                      KC_RABK, KC_RCBR, KC_COMM,  KC_DOT, KC_BSLS, KC_TILD,\
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                 KC_LALT,   LOWER,  KC_SPC, KC_LCTL,    KC_LGUI,  KC_ENT,  RAISE,  KC_RCTL \
+                                 KC_LALT,   LOWER,  KC_SPC, KC_LCTL,    KC_LGUI,   RAISE,  KC_ENT, KC_RCTL \
                              //`-----------------------------------'  `-----------------------------------'
   ),
 
   [_LOWER] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_LGUI,\
+       KC_GRV, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_PLUS,  KC_EQL,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_TAB,   KC_F2,   KC_F3,   KC_F8,  KC_F10,  KC_F11,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, KC_BSPC,\
+       KC_TAB,   KC_F2,   KC_F3,   KC_F8,  KC_F10,  KC_F11,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT,  KC_DOT, KC_BSPC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_4, KC_KP_5,                      KC_KP_6, KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_0, KC_CAPS,\
+      KC_LSFT,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_LGUI,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                 KC_LALT,   LOWER,  KC_SPC, KC_LCTL,    KC_LGUI,  KC_ENT,  RAISE,  KC_RCTL \
+                                 KC_LALT,   LOWER,  KC_SPC, KC_LCTL,    KC_LGUI,   RAISE,  KC_ENT, KC_RCTL \
+                             //`-----------------------------------'  `-----------------------------------'
+  ),
+
+  [_RAISE] = LAYOUT( \
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+      KC_GRV, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_PLUS,  KC_EQL,\
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+       KC_TAB, KC_LABK, KC_LCBR, KC_LBRC, KC_LPRN, KC_QUOT,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, KC_UNDS, KC_PIPE,\
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LSFT, KC_RABK, KC_RCBR, KC_RBRC, KC_RPRN, KC_DQUO,                      KC_TILD, KC_BSLS, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,\
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                 KC_LALT,   LOWER,  KC_SPC, KC_LCTL,    KC_LGUI,   RAISE,  KC_ENT, KC_RCTL \
                              //`-----------------------------------'  `-----------------------------------'
   ),
 };
